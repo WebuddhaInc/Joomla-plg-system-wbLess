57a1883b913f89635043fae633713471087f56c1
David Hunt <bitbucket.davidhunt@holodyn.com>
2016-07-07 18:18:45 -0400

    Added taret_scope flag.

--- plugin/wbless.php
+++ plugin/wbless.php
@@ -68,209 +68,214 @@ class plgSystemWbLess extends JPlugin {
    */
   private function _execute(){
 
-    $app = JFactory::getApplication();
-    if( $app->isAdmin() ){
+    // Stage
+      $app = JFactory::getApplication();
+      $trigger_scope = $this->params->get('trigger_scope', 'client');
 
-      // Skip admin requests
+    // Bail
+      if (
+        $app->isAdmin()
+        ? !in_array($trigger_scope, array('all', 'admin'))
+        : !in_array($trigger_scope, array('all', 'client'))
+        ) {
 
-    }
-    else {
+        return;
 
-      // Trigger Domains
-        $trigger_domains = explode("\r\n", $this->params->get('trigger_domains'));
-        if( !empty($trigger_domains) ){
-          $domains_match = 0;
-          $domains_valid = 0;
-          foreach( $trigger_domains AS $trigger_domain ){
-            $trigger_domain = trim($trigger_domain);
-            if( !empty($trigger_domain) ){
-              $domains_valid++;
-              if( $_SERVER['SERVER_NAME'] == $trigger_domain ){
-                $domains_match++;
-              }
-            }
-          }
-          if( $domains_valid && !$domains_match ){
-            return;
-          }
-        }
+      }
 
-      // Find Watch Paths
-        $watch_paths  = explode("\r\n", $this->params->get('watch_paths'));
-        $watch_config = array();
-        $template = JFactory::getApplication()->getTemplate();
-        for($i=0;$i<count($watch_paths);$i++){
-          $watch_path = trim(preg_replace('/[\r\n]/','',preg_replace('/\{\$template\}/', $template, $watch_paths[$i])));
-          if( strlen($watch_path) ){
-            $watch_paths[$i] = $watch_path;
-            $abs_path  = JPATH_BASE . DIRECTORY_SEPARATOR . $watch_path;
-            if( $abs_path != JPATH_BASE . DIRECTORY_SEPARATOR && is_dir($abs_path) ){
-              $watch_config[ $watch_path ] = array(
-                'abs_path' => $abs_path
-                );
+    // Trigger Domains
+      $trigger_domains = explode("\r\n", $this->params->get('trigger_domains'));
+      if( !empty($trigger_domains) ){
+        $domains_match = 0;
+        $domains_valid = 0;
+        foreach( $trigger_domains AS $trigger_domain ){
+          $trigger_domain = trim($trigger_domain);
+          if( !empty($trigger_domain) ){
+            $domains_valid++;
+            if( $_SERVER['SERVER_NAME'] == $trigger_domain ){
+              $domains_match++;
             }
           }
         }
-
-      // Process
-        if( class_exists('\WebuddhaInc\LessMonitor') ){
-          (new \WebuddhaInc\LessMonitor(array(
-            'base_path'          => JPATH_ROOT . '/',
-            'watch_paths'        => $watch_paths,
-            'dependency_exclude' => $this->params->get('dependency_exclude', 0),
-            'compress'           => $this->params->get('compress', 0)
-            )))
-            ->execute();
-          return true;
+        if( $domains_valid && !$domains_match ){
+          return;
         }
+      }
 
-      // Examing dependencies
-        $lessDependent = array();
-        $partialMTimes = array();
-        foreach($watch_config AS $watch_path => $watch_path_config){
-          $abs_path = $watch_path_config['abs_path'];
-          $lessrc_files = self::_find_files( $abs_path, '/\.lessrc$/' );
-          if( $lessrc_files ){
-            foreach( $lessrc_files AS $lessrc_file ){
-              $source_path = $lessrc_file['path'] . DIRECTORY_SEPARATOR;
-              $source_file = $lessrc_file['name'];
-              $lessrc_json = json_decode( file_get_contents($source_path . $source_file) );
-              if( is_object($lessrc_json) ){
-                $files  = isset($lessrc_json->files) ? $lessrc_json->files : array($lessrc_json);
-                $shared = isset($lessrc_json->import) ? $lessrc_json->import : array();
-                foreach( $files AS $file ){
-                  $dependentFile = $source_path . ($lessrc_file['name'] == '.lessrc' ? '*' : substr($lessrc_file['name'], 0, strlen($lessrc_file['name'])-2));
-                  if( isset($file->file) && is_string($file->file) ){
-                    $dependentFile = $source_path . $file->file;
-                  }
-                  $importLookupList = ( isset($file->import) && is_array($file->import) ? $file->import : array() );
-                  $importLookupList = array_merge( $importLookupList, $shared );
-                  foreach( $importLookupList AS $import ){
-                    $target_file = self::normalizePath( $source_path . $import );
-                    if( is_readable($target_file) ){
-                      if( empty($lessDependent[ $dependentFile ]) ){
-                        $lessDependent[ $dependentFile ] = array();
-                      }
-                      if( empty($partialMTimes[$target_file]) ){
-                        $partialMTimes[$target_file] = filemtime($target_file);
-                      }
-                      if( empty($lessDependent[ $dependentFile ][$target_file]) ){
-                        $lessDependent[ $dependentFile ][ $target_file ] = $partialMTimes[$target_file];
-                      }
+    // Find Watch Paths
+      $watch_paths  = explode("\r\n", $this->params->get('watch_paths'));
+      $watch_config = array();
+      $template = JFactory::getApplication()->getTemplate();
+      for($i=0;$i<count($watch_paths);$i++){
+        $watch_path = trim(preg_replace('/[\r\n]/','',preg_replace('/\{\$template\}/', $template, $watch_paths[$i])));
+        if( strlen($watch_path) ){
+          $watch_paths[$i] = $watch_path;
+          $abs_path  = JPATH_BASE . DIRECTORY_SEPARATOR . $watch_path;
+          if( $abs_path != JPATH_BASE . DIRECTORY_SEPARATOR && is_dir($abs_path) ){
+            $watch_config[ $watch_path ] = array(
+              'abs_path' => $abs_path
+              );
+          }
+        }
+      }
+
+    // Process
+      if( class_exists('\WebuddhaInc\LessMonitor') ){
+        (new \WebuddhaInc\LessMonitor(array(
+          'base_path'          => JPATH_ROOT . '/',
+          'watch_paths'        => $watch_paths,
+          'dependency_exclude' => $this->params->get('dependency_exclude', 0),
+          'compress'           => $this->params->get('compress', 0)
+          )))
+          ->execute();
+        return true;
+      }
+
+    // Examing dependencies
+      $lessDependent = array();
+      $partialMTimes = array();
+      foreach($watch_config AS $watch_path => $watch_path_config){
+        $abs_path = $watch_path_config['abs_path'];
+        $lessrc_files = self::_find_files( $abs_path, '/\.lessrc$/' );
+        if( $lessrc_files ){
+          foreach( $lessrc_files AS $lessrc_file ){
+            $source_path = $lessrc_file['path'] . DIRECTORY_SEPARATOR;
+            $source_file = $lessrc_file['name'];
+            $lessrc_json = json_decode( file_get_contents($source_path . $source_file) );
+            if( is_object($lessrc_json) ){
+              $files  = isset($lessrc_json->files) ? $lessrc_json->files : array($lessrc_json);
+              $shared = isset($lessrc_json->import) ? $lessrc_json->import : array();
+              foreach( $files AS $file ){
+                $dependentFile = $source_path . ($lessrc_file['name'] == '.lessrc' ? '*' : substr($lessrc_file['name'], 0, strlen($lessrc_file['name'])-2));
+                if( isset($file->file) && is_string($file->file) ){
+                  $dependentFile = $source_path . $file->file;
+                }
+                $importLookupList = ( isset($file->import) && is_array($file->import) ? $file->import : array() );
+                $importLookupList = array_merge( $importLookupList, $shared );
+                foreach( $importLookupList AS $import ){
+                  $target_file = self::normalizePath( $source_path . $import );
+                  if( is_readable($target_file) ){
+                    if( empty($lessDependent[ $dependentFile ]) ){
+                      $lessDependent[ $dependentFile ] = array();
+                    }
+                    if( empty($partialMTimes[$target_file]) ){
+                      $partialMTimes[$target_file] = filemtime($target_file);
+                    }
+                    if( empty($lessDependent[ $dependentFile ][$target_file]) ){
+                      $lessDependent[ $dependentFile ][ $target_file ] = $partialMTimes[$target_file];
                     }
                   }
                 }
               }
-              else {
-                throw new Exception('Error parsing contents of ' . $source_path . $source_file);
-              }
+            }
+            else {
+              throw new Exception('Error parsing contents of ' . $source_path . $source_file);
             }
           }
         }
+      }
 
-      // Look for Less Files & Process
-        $lessProcessed = array();
-        foreach($watch_config AS $watch_path => $watch_path_config){
-          $abs_path = $watch_path_config['abs_path'];
-          $less_files = self::_find_files( $abs_path, '/\.less$/' );
-          if( $less_files ){
-            $target_base = $abs_path;
-            if( substr($target_base, strlen($target_base) - 5) == ('less'.DIRECTORY_SEPARATOR) ){
-              $target_base = substr($target_base, 0, strlen($target_base) - 5) . 'css' . DIRECTORY_SEPARATOR;
-            }
-            foreach( $less_files AS $less_file ){
-              $source_path = $less_file['path'] . DIRECTORY_SEPARATOR;
-              $source_file = $less_file['name'];
-              if( substr($source_file,0,1) != '_' ){
-                $target_path = $target_base . substr($source_path, strlen($abs_path));
-                if( substr($target_path, strlen($target_path) - 5) == ('less'.DIRECTORY_SEPARATOR) ){
-                  $target_path = substr($target_path, 0, strlen($target_path) - 5) . 'css' . DIRECTORY_SEPARATOR;
-                }
-                $target_file = preg_replace('/\.less$/','.css',$less_file['name']);
-                if( is_dir($target_path) ){
-                  if( is_readable($target_path.$target_file) ){
-                    $source_filemtime = filemtime($source_path.$source_file);
-                    $target_filemtime = filemtime($target_path.$target_file);
-                    if( $source_filemtime < $target_filemtime ){
-                      $less_is_more = true;
-                      $less_matches = (
-                        isset($lessDependent[$source_path.$source_file])
-                        ? $lessDependent[$source_path.$source_file]
-                        : array()
-                        );
-                      if( empty($less_matches) || !$this->params->get('dependency_exclude', 0) ){
-                        $less_matches = array_merge($less_matches, (
-                              isset($lessDependent[$source_path.'*'])
-                              ? $lessDependent[$source_path.'*']
-                              : array()
-                              ));
-                      }
-                      if( !empty($less_matches) ){
-                        foreach( $less_matches AS $dependencyFile => $dependencyFileTime ){
-                          if( $target_filemtime < $dependencyFileTime ){
-                            $less_is_more = false;
-                            break;
-                          }
+    // Look for Less Files & Process
+      $lessProcessed = array();
+      foreach($watch_config AS $watch_path => $watch_path_config){
+        $abs_path = $watch_path_config['abs_path'];
+        $less_files = self::_find_files( $abs_path, '/\.less$/' );
+        if( $less_files ){
+          $target_base = $abs_path;
+          if( substr($target_base, strlen($target_base) - 5) == ('less'.DIRECTORY_SEPARATOR) ){
+            $target_base = substr($target_base, 0, strlen($target_base) - 5) . 'css' . DIRECTORY_SEPARATOR;
+          }
+          foreach( $less_files AS $less_file ){
+            $source_path = $less_file['path'] . DIRECTORY_SEPARATOR;
+            $source_file = $less_file['name'];
+            if( substr($source_file,0,1) != '_' ){
+              $target_path = $target_base . substr($source_path, strlen($abs_path));
+              if( substr($target_path, strlen($target_path) - 5) == ('less'.DIRECTORY_SEPARATOR) ){
+                $target_path = substr($target_path, 0, strlen($target_path) - 5) . 'css' . DIRECTORY_SEPARATOR;
+              }
+              $target_file = preg_replace('/\.less$/','.css',$less_file['name']);
+              if( is_dir($target_path) ){
+                if( is_readable($target_path.$target_file) ){
+                  $source_filemtime = filemtime($source_path.$source_file);
+                  $target_filemtime = filemtime($target_path.$target_file);
+                  if( $source_filemtime < $target_filemtime ){
+                    $less_is_more = true;
+                    $less_matches = (
+                      isset($lessDependent[$source_path.$source_file])
+                      ? $lessDependent[$source_path.$source_file]
+                      : array()
+                      );
+                    if( empty($less_matches) || !$this->params->get('dependency_exclude', 0) ){
+                      $less_matches = array_merge($less_matches, (
+                            isset($lessDependent[$source_path.'*'])
+                            ? $lessDependent[$source_path.'*']
+                            : array()
+                            ));
+                    }
+                    if( !empty($less_matches) ){
+                      foreach( $less_matches AS $dependencyFile => $dependencyFileTime ){
+                        if( $target_filemtime < $dependencyFileTime ){
+                          $less_is_more = false;
+                          break;
                         }
                       }
-                      if( $less_is_more ){
-                        continue;
-                      }
+                    }
+                    if( $less_is_more ){
+                      continue;
                     }
                   }
-                  $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
-                  $this->_compileLessFile(
-                      array('path' => $source_path, 'file' => $source_file),
-                      array('path' => $target_path, 'file' => $target_file)
-                      );
-
-                  /*
-                   * INC PARSER WRAPPER
-                   *
-                  if( empty($lessParser) ){
-                    require_once 'lessc/lessc.inc.php';
-                    $lessCompiler = new lessc();
-                    if( $this->params->get('compress', 0) ){
-                      $lessCompiler->setFormatter('compressed');
-                    }
-                  }
-                  if( isset($lessCompiler) ){
-                    $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
-                    $lessCompiler->compileFile( $source_path.$source_file, $target_path.$target_file );
-                  }
-                   */
-
-                  /*
-                   * LIB PARSER DIRECTLY
-                   *
-                    if( empty($lessParser) ){
-                      if( !class_exists('Less_Parser') ){
-                        require_once 'lessc/lib/Less/Autoloader.php';
-                        Less_Autoloader::register();
-                      }
-                      $lessParser = new Less_Parser(array(
-                        'compress' => $this->params->get('compress', 0)
-                        ));
-                    }
-                    if( isset($lessParser) ){
-                      $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
-                      $lessParser->parseFile( $source_path.$source_file, $source_path );
-                      file_put_contents( $target_path.$target_file, $lessParser->getCss() );
-                    }
-                  */
-
                 }
+                $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
+                $this->_compileLessFile(
+                    array('path' => $source_path, 'file' => $source_file),
+                    array('path' => $target_path, 'file' => $target_file)
+                    );
+
+                /*
+                 * INC PARSER WRAPPER
+                 *
+                if( empty($lessParser) ){
+                  require_once 'lessc/lessc.inc.php';
+                  $lessCompiler = new lessc();
+                  if( $this->params->get('compress', 0) ){
+                    $lessCompiler->setFormatter('compressed');
+                  }
+                }
+                if( isset($lessCompiler) ){
+                  $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
+                  $lessCompiler->compileFile( $source_path.$source_file, $target_path.$target_file );
+                }
+                 */
+
+                /*
+                 * LIB PARSER DIRECTLY
+                 *
+                  if( empty($lessParser) ){
+                    if( !class_exists('Less_Parser') ){
+                      require_once 'lessc/lib/Less/Autoloader.php';
+                      Less_Autoloader::register();
+                    }
+                    $lessParser = new Less_Parser(array(
+                      'compress' => $this->params->get('compress', 0)
+                      ));
+                  }
+                  if( isset($lessParser) ){
+                    $lessProcessed[] = array($source_path.$source_file, $target_path.$target_file);
+                    $lessParser->parseFile( $source_path.$source_file, $source_path );
+                    file_put_contents( $target_path.$target_file, $lessParser->getCss() );
+                  }
+                */
+
               }
             }
           }
         }
+      }
 
-      // Debug
-        if( !empty($debug) ){
-          inspect( $watch_config, $lessDependent, $lessProcessed );
-        }
-
-    }
+    // Debug
+      if( !empty($debug) ){
+        inspect( $watch_config, $lessDependent, $lessProcessed );
+      }
 
   }
 
